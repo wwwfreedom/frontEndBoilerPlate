67a14cf4c2ce2bb36fc4327a01c6f13be57bdd21
KevWork <kevin.truong@sensis.com.au>
2018-06-21 17:18:38 +1000

    add styled-component setup

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const rewireReactHotLoader = require("react-app-rewire-hot-loader")
 const { injectBabelPlugin } = require("react-app-rewired")
 
 module.exports = function override(config, env) {
+  config = injectBabelPlugin("babel-plugin-styled-components", config)
   config = rewireReactHotLoader(config, env)
   return config
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import baseStyle from "./styles/index"
+
 import registerServiceWorker from "./registerServiceWorker"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const render = () => {
+  baseStyle()
+  ReactDOM.render(<App />, document.getElementById("root"))
+}
+
+render()
+
 registerServiceWorker()
