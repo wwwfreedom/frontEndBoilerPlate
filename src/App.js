e3168e87897f5f4c0ef190bcea6ba8ecc7feea2e
KevWork <kevin.truong@sensis.com.au>
2018-06-22 09:57:40 +1000

    convert to styles to styled-component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React, { Component } from "react"
 import { hot } from "react-hot-loader"
-import logo from "./logo.svg"
-import "./App.css"
+import styled, { keyframes } from "styled-components"
+import logoSource from "./logo.svg"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <Wrapper>
+        <Header>
+          <Logo src={logoSource} alt="logo" />
+          <Title>Welcome to React</Title>
+        </Header>
+        <Paragraph>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </Paragraph>
+      </Wrapper>
     )
   }
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const Header = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const Title = styled.h1`
+  font-size: 1.5em;
+`
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Logo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+  animation: ${rotate360} infinite 20s linear;
+`
+
+const Paragraph = styled.p`
+  font-size: large;
+`
 
 export default hot(module)(App)
